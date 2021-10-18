80ed545588bbcc29e5fecdb7cfb1e7ba8cec4df1
Raihan Kemmy Rachmansyah <raikemmy02@gmail.com>
2021-10-18 17:32:22 +0700

    Fixing Dropdown

--- Components/dropDown/index.jsx
+++ Components/dropDown/index.jsx
@@ -9,12 +9,12 @@ export default function DropDownEdit({className, value, onChange}) {
   return (
     <Dropdown
       options={kota}
-      placeholder="Filter"
-      className={`text-center ${className}  ${styles.dropDown}`}
+      placeholder="Pilih Provinsi"
+      className={`text-center ${className} ${styles.dropDown}`}
       controlClassName={`text-xl font-medium ${styles.dropDownControl}`}
       menuClassName={`font-medium ${styles.menuControl}`}
       arrowClassName={`${styles.myArrowClassName}`}
-      arrowOpen={<span className="border-2 border-red-500"/>}
+      arrowOpen={<span className={`border-2 border-red-500 ${styles.dropDownControl}`}/>}
       value={value}
       onChange={(e) => onChange(e.value)}
     />
