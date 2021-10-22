787ca41c5b37c9475c2d504ee2e79371c9a3b0fa
Raihan Kemmy Rachmansyah <raikemmy02@gmail.com>
2021-10-22 09:25:06 +0700

    DB Vaksinasi Kedua

---
+++ models/vaksinasiprovinsikedua.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const VaksinasiProvinsiKeduaSchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true,
+    },
+    nama: {
+        type: String,
+        required: true,
+    },
+})
+
+module.exports = mongoose.models.VaksinasiProvinsiKedua || mongoose.model('VaksinasiProvinsiKedua', VaksinasiProvinsiKeduaSchema);
