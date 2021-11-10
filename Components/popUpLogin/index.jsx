bee9988f267ba2c128f4580bcdc00631d6edec6e
FransiscoReadyPermana <fransiscoreadyy@gmail.com>
2021-11-10 18:46:40 +0700

    making popup login

--- Components/popUpLogin/index.jsx
+++ Components/popUpLogin/index.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import Link from 'next/link';
-import style from './popUpLogin.module.css'
-import Close from '../icons/Close';
+import React, { useState } from "react";
+import Link from "next/link";
+import style from "./popUpLogin.module.css";
+import Close from "../icons/Close";
+import EyeShow from "../icons/EyeShow";
+import EyeHide from "../icons/EyeHide";
 
-export default function PopUpLogin({open, onClick}) {
+export default function PopUpLogin({ open, onClick }) {
   if (!open) {
     return null;
   }
+
+  const [showPassword, setShowPassword] = useState(true);
   return (
-    <div id="background" className={`absolute h-screen w-screen flex items-center justify-center z-20 top-0 left-0 ${style.background}`}>
-      <div className="container" className={`relative bg-white py-10 px-20 ${style.container}`}>
-        <button className={`absolute right-8 top-8 rounded-full ${style.close}`} onClick={onClick}>
-          <Close/>
+    <div
+      id="background"
+      className={`absolute h-screen w-screen flex items-center justify-center z-20 top-0 left-0 ${style.background}`}
+    >
+      <div
+        className="container"
+        className={`relative bg-white py-10 px-20 ${style.container}`}
+      >
+        <button
+          className={`absolute right-8 top-8 rounded-full ${style.close}`}
+          onClick={onClick}
+        >
+          <Close />
         </button>
-        <h1 className={`text-center text-dark-grey mb-6 ${style.judul}`}>MASUK</h1>
+        <h1 className={`text-center text-dark-grey mb-6 ${style.judul}`}>
+          MASUK
+        </h1>
         <form action="#" className={`flex flex-col gap-6 ${style.form}`}>
           <div id="email">
             <label htmlFor="email">Email</label>
@@ -21,16 +36,31 @@ export default function PopUpLogin({open, onClick}) {
           </div>
           <div id="password">
             <label htmlFor="password">Kata Sandi</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Masukkan Kata Sandi"
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Masukkan Kata Sandi"
+              />
+              <button
+                className="absolute right-3 top-5"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeHide /> : <EyeShow />}  
+              </button>
+            </div>
           </div>
-          <input type="submit" value="Masuk" className="bg-purple text-white py-3 rounded-3xl"/>
+          <input
+            type="submit"
+            value="Masuk"
+            className="bg-purple text-white py-3 rounded-3xl"
+          />
         </form>
         <h6 className={`mt-4 text-center font-semibold ${style.text}`}>
-          Belum Memiliki Akun? <span className="text-purple"><Link href="/vaksinasi">Daftar Sekarang</Link></span> 
+          Belum Memiliki Akun?{" "}
+          <span className="text-purple">
+            <Link href="/vaksinasi">Daftar Sekarang</Link>
+          </span>
         </h6>
       </div>
     </div>
