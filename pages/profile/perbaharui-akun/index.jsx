bc26676fb389590bd8c336b345306a8323180046
Raihan Kemmy Rachmansyah <raikemmy02@gmail.com>
2021-11-20 20:16:29 +0700

    Page Profile

--- pages/profile/perbaharui-akun/index.jsx
+++ pages/profile/perbaharui-akun/index.jsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import uuid from "react-uuid";
-import styles from "../akun.module.css";
+import styles from "../edit.module.css";
 import Footer from "../../../Components/footer";
 import Button from "../../../Components/button";
 
-export default function AkunSaya({ data }) {
+export default function EditAkun({ data }) {
   return (
     <div className="h-screen w-full">
       <section
@@ -37,7 +37,9 @@ export default function AkunSaya({ data }) {
               >
                 <div
                   id="icon"
-                  className={`relative h-7 w-7 flex items-center justify-center`}
+                  className={
+                    "relative h-7 w-7 flex items-center justify-center"
+                  }
                 >
                   <Image
                     src="/images/Profile_Active.svg"
@@ -120,101 +122,116 @@ export default function AkunSaya({ data }) {
                 color="dark-grey"
                 className="text-center font-semibold text-2xl text-dark-grey"
               >
-                Akun Saya
+                Perbaharui Akun
               </p>
+              <div
+                id="button"
+                className="flex flex-row items-center justify-between gap-8 w-1/3"
+              >
+                <Link href={`/profile/akun-saya`} passHref>
+                  <button
+                    className={`w-full rounded-full py-2 h-full text-l bg-white ${styles.button_batal}`}
+                  >
+                    Batal
+                  </button>
+                </Link>
 
-              <Link href={`/profile/perbaharui-akun`} passHref>
                 <button
-                  className={`w-1/4 rounded-full py-2 h-full text-l text-white bg-purple ${styles.button}`}
+                  className={`w-full rounded-full py-2 h-full text-l text-white bg-purple ${styles.button}`}
                 >
-                  Perbaharui Akun
+                  Simpan
                 </button>
-              </Link>
+              </div>
             </div>
 
             <hr className="h-1 bg-dark-grey w-full mb-6 mt-6 opacity-25" />
 
-            <div id="email" className="flex flex-col">
-              <p
-                color="dark-grey"
-                className="text-left font-semibold text-l flex items-center text-dark-grey"
-              >
-                Email
-              </p>
+            <form
+              action="#"
+              className={`flex flex-col w-full pr-8 ${styles.form}`}
+            >
+              <div id="email" className="flex flex-col">
+                <p
+                  color="dark-grey"
+                  className="text-left font-semibold text-l flex items-center text-dark-grey"
+                >
+                  Email
+                </p>
 
-              <p
-                color="dark-grey"
-                className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
-              >
-                raikemmy@upnvj.ac.id
-              </p>
-            </div>
+                <p
+                  color="dark-grey"
+                  className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
+                >
+                  raikemmy@upnvj.ac.id
+                </p>
+              </div>
 
-            <div id="nama" className="flex flex-col mt-6">
-              <p
-                color="dark-grey"
-                className="text-left font-semibold text-l flex items-center text-dark-grey"
-              >
-                Nama Lengkap
-              </p>
+              <div id="nama" className="flex flex-col mt-6">
+                <label
+                  htmlFor="nama-lengkap"
+                  className="text-left font-semibold text-l flex items-center text-dark-grey"
+                >
+                  Nama Lengkap
+                </label>
 
-              <p
-                color="dark-grey"
-                className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
-              >
-                Raihan Kemmy Rachmansyah
-              </p>
-            </div>
+                <input
+                  type="text"
+                  id="nama-lengkap"
+                  value="Raihan Kemmy Rachmansyah"
+                  className={`rounded-full w-full text-xl ml-4 ${styles.input}`}
+                />
+              </div>
 
-            <div id="tanggal-lahir" className="flex flex-col mt-6">
-              <p
-                color="dark-grey"
-                className="text-left font-semibold text-l flex items-center text-dark-grey"
-              >
-                Tanggal Lahir
-              </p>
+              <div id="tanggal-lahir" className="flex flex-col mt-6">
+                <label
+                  htmlFor="tanggal"
+                  className="text-left font-semibold text-l flex items-center text-dark-grey"
+                >
+                  Tanggal Lahir
+                </label>
 
-              <p
-                color="dark-grey"
-                className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
-              >
-                05 Januari 2001
-              </p>
-            </div>
+                <input
+                  type="text"
+                  id="tanggal"
+                  value="05 Januari 2001"
+                  className={`rounded-full w-full text-xl text-dark-grey ml-4 ${styles.input}`}
+                />
+              </div>
 
-            <div id="jenis-kelamin" className="flex flex-col mt-6">
-              <p
-                color="dark-grey"
-                className="text-left font-semibold text-l flex items-center text-dark-grey"
-              >
-                Jenis Kelamin
-              </p>
+              <div id="jenis-kelamin" className="flex flex-col mt-6">
+                <p
+                  color="dark-grey"
+                  className="text-left font-semibold text-l flex items-center text-dark-grey"
+                >
+                  Jenis Kelamin
+                </p>
 
-              <p
-                color="dark-grey"
-                className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
-              >
-                Laki-Laki
-              </p>
-            </div>
+                <p
+                  color="dark-grey"
+                  className="text-left font-normal text-xl flex items-center mt-2 ml-4 text-dark-grey"
+                >
+                  Laki-Laki
+                </p>
+              </div>
 
-            <div id="alamat" className="flex flex-col mt-6">
-              <p
-                color="dark-grey"
-                className="text-left font-semibold text-l flex items-center text-dark-grey"
-              >
-                Alamat
-              </p>
+              <div id="alamat-tinggal" className="flex flex-col mt-6">
+                <label
+                  htmlFor="alamat"
+                  className="text-left font-semibold text-l flex items-center text-dark-grey"
+                >
+                  Alamat
+                </label>
 
-              <p
-                color="dark-grey"
-                className="text-left font-normal text-xl flex items-center mt-2 ml-4 mr-8 text-dark-grey text-justify"
-              >
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus sed vitae maxime illum, repellendus quo qui numquam
-                in a, et sapiente cumque eum. Quaerat, nesciunt.
-              </p>
-            </div>
+                <input
+                  type="text"
+                  id="alamat"
+                  value="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatibus sed vitae maxime illum, repellendus quo qui numquam
+                  in a, et sapiente cumque eum. Quaerat, nesciunt."
+                  className={`rounded-full w-full text-xl text-dark-grey ml-4 ${styles.input}`}
+                />
+              </div>
+            </form>
           </div>
         </div>
       </section>
