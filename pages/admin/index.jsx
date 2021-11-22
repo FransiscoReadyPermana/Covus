323d4a6c5c5c52df05b1ee8aa3d945cb9d9aeee9
fransiscoready <fransiscoreadyy@gmail.com>
2021-11-22 15:20:24 +0700

    making login

---
+++ pages/admin/index.jsx
@@ -0,0 +1,38 @@
+import { getSession } from 'next-auth/client';
+
+export default function Admin({ user }) {
+  console.log('ini dari session ' + user);
+  if (
+    user.name !== "admin" &&
+    user.email !== process.env.ADMIN_EMAIL
+  ) {
+    return (
+      <div className="pt-40">
+        <p>You are not authorized to access this page</p>;
+      </div>
+    );
+  }
+  return (
+    <div className="pt-40">
+      <p>berhasil</p>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+  const user = session.user;
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { user },
+  };
+}
